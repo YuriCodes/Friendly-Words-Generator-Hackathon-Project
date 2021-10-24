6929ec95458c58df8d692f6ec108c71e0302352a
yuri <yuricodesbot@gmail.com>
2021-10-24 04:19:59 -0400

    first commit

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ let quotes = [
 //for image at the end of each quote
 var img = document.createElement("img");
 img.src= "sunflower.png"
-//var src= document.getElementById('quote');
-//src.appendChild(img);
+var src= document.getElementById('quote');
+src.appendChild(img);
 
 //for the quotes
 function newQuote(){
